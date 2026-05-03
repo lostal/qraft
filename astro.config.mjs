cc0d894b02587bb8779930e8d637080b14fb71fd
lostal <alvarolostal04@gmail.com>
2026-05-03 12:13:57 +0200

    og image

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  // TODO: cambiar antes del deploy a producción
+  site: 'https://TODO-set-domain.example',
   integrations: [react(), tailwind()],
   output: 'static',
   vite: {
